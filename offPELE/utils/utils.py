3262fa7e82ba91e4fbc8b67c0621bf52c697ddc0
Marti Municoy <martimunicoy@gmail.com>
2020-05-28 16:01:58 +0200

    Add data to run examples

---
+++ offPELE/utils/utils.py
@@ -0,0 +1,15 @@
+from pkg_resources import resource_filename
+import os
+
+
+def get_data_file_path(relative_path):
+
+    fn = resource_filename('offPELE', os.path.join(
+        'data', relative_path))
+
+    if not os.path.exists(fn):
+        raise ValueError(
+            "Sorry! {fn} does not exist. If you just added it, you'll have to "
+            + " re-install")
+
+    return fn
