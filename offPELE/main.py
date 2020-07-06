c69643f94333753a59f22b3f2aae13498bf6d7fa
Daniel <daniel.soler@e-campus.uab.cat>
2020-07-06 16:06:51 +0200

    Main function development

--- offPELE/main.py
+++ offPELE/main.py
@@ -14,15 +14,17 @@ __email__ = "marti.municoy@bsc.es"
 
 def parse_args():
     parser = ap.ArgumentParser()
-    parser.add_argument("pdb_file", metavar="FILE", type=str,
+    parser.add_argument("pdb_file", metavar="PDB FILE", type=str,
                         help="Path PDB file to parameterize")
     args = parser.parse_args()
 
-    return args.pdb_file
+    return args
 
 
 def main():
-    pass
+    args = parse_args()
+    print('hey')
+    print(args.pdb_file)
 
 
 if __name__ == '__main__':
