583c2f8c3adfcaa99cec20e0a03ba02c571b3aa2
Marti Municoy <martimunicoy@gmail.com>
2020-07-16 11:42:35 +0200

    Add docstrings to topology module

--- offpele/topology/topology.py
+++ offpele/topology/topology.py
@@ -1,17 +1,49 @@
-# Global imports
+"""
+This module handles the topological elements of force fields.
+"""
+
 from simtk import unit
 
 
 class TopologyElement(object):
+    """
+    A wrapper for any topological element.
+    """
+
     _name = None
     _writable_attrs = []
 
     class TopologyIterator(object):
+        """
+        An iterator for topological elements that iterates over their
+        attributes in an ordered way.
+
+        It is useful when writing topological elements to file.
+        """
+
         def __init__(self, top_el):
+            """
+            It initiates a TopologyIterator object.
+
+            Parameters
+            ----------
+            top_el : a TopologyElement object
+                The topology element to iterate on.
+            """
             self._index = int(0)
             self._top_el = top_el
 
         def __next__(self):
+            """
+            It returns the next item for the iteration.
+
+            Returns
+            -------
+            attr_name : str
+                The name of the attribute
+            attr_value : float
+                The value of the attribute
+            """
             if self._index == len(self._top_el._writable_attrs):
                 raise StopIteration
 
@@ -23,16 +55,48 @@ class TopologyElement(object):
 
     @property
     def name(self):
+        """
+        The name that this topological element has.
+
+        Returns
+        -------
+        name : str
+            The name of the topological element
+        """
         return self._name
 
     @property
     def n_writable_attrs(self):
+        """
+        The number of writable attributes this topological element has.
+
+        Returns
+        -------
+        n_writable_attrs : int
+            The number of writable attributes
+        """
         return len(self._writable_attrs)
 
     def __iter__(self):
+        """
+        It returns an instance of the TopologyIterator.
+
+        Returns
+        -------
+        iterator : a TopologyIterator
+            The TopologyIterator object
+        """
         return self.TopologyIterator(self)
 
     def __repr__(self):
+        """
+        It returns the representation string of this topological element.
+
+        Returns
+        -------
+        repr_string : str
+            The representation string
+        """
         repr_string = '{}('.format(self._name)
         attrs = [attr for attr in self]
         for attr_name, value in attrs[:-1]:
@@ -42,15 +106,44 @@ class TopologyElement(object):
         return repr_string
 
     def __str__(self):
+        """
+        It returns the readable representation string of this topological
+        element.
+
+        Returns
+        -------
+        str_string : str
+            The readable representation string
+        """
         return self.__repr__()
 
 
 class Bond(TopologyElement):
+    """
+    It represents a bond in the topology.
+    """
+
     _name = 'Bond'
     _writable_attrs = ['atom1_idx', 'atom2_idx', 'spring_constant', 'eq_dist']
 
     def __init__(self, index=-1, atom1_idx=None, atom2_idx=None,
                  spring_constant=None, eq_dist=None):
+        """
+        It initiates a Bond object.
+
+        Parameters
+        ----------
+        index : int
+            The index of this Bond object
+        atom1_idx : int
+            The index of the first atom involved in this Bond
+        atom2_idx : int
+            The index of the second atom involved in this Bond
+        spring_constant : simtk.unit.Quantity
+            The spring constant of this Bond
+        eq_dist : simtk.unit.Quantity
+            The equilibrium distance of this Bond
+        """
         self._index = index
         self._atom1_idx = atom1_idx
         self._atom2_idx = atom2_idx
@@ -58,39 +151,117 @@ class Bond(TopologyElement):
         self._eq_dist = eq_dist
 
     def set_atom1_idx(self, index):
+        """
+        It sets atom1's index.
+
+        Parameters
+        ----------
+        index : int
+            The index of the first atom involved in this Bond
+        """
         self._atom1_idx = index
 
     def set_atom2_idx(self, index):
+        """
+        It sets atom2's index.
+
+        Parameters
+        ----------
+        index : int
+            The index of the second atom involved in this Bond
+        """
         self._atom2_idx = index
 
     @property
     def index(self):
+        """
+        Bond's index.
+
+        Returns
+        -------
+        index : int
+            The index of this Bond object
+        """
         return self._index
 
     @property
     def atom1_idx(self):
+        """
+        Bond's atom1 index.
+
+        Returns
+        -------
+        atom1_idx : int
+            The index of the first atom involved in this Bond object
+        """
         return self._atom1_idx
 
     @property
     def atom2_idx(self):
+        """
+        Bond's atom2 index.
+
+        Returns
+        -------
+        atom2_idx : int
+            The index of the second atom involved in this Bond object
+        """
         return self._atom2_idx
 
     @property
     def spring_constant(self):
+        """
+        Bond's spring constant.
+
+        Returns
+        -------
+        spring_constant : simtk.unit.Quantity
+            The spring constant of this Bond object
+        """
         return self._spring_constant
 
     @property
     def eq_dist(self):
+        """
+        Bond's equilibrium distance.
+
+        Returns
+        -------
+        eq_dist : simtk.unit.Quantity
+            The equilibrium distance of this Bond object
+        """
         return self._eq_dist
 
 
 class Angle(TopologyElement):
+    """
+    It represents an angle in the topology.
+    """
+
     _name = 'Angle'
     _writable_attrs = ['atom1_idx', 'atom2_idx', 'atom3_idx',
                        'spring_constant', 'eq_angle']
 
     def __init__(self, index=-1, atom1_idx=None, atom2_idx=None,
                  atom3_idx=None, spring_constant=None, eq_angle=None):
+        """
+        It initiates an Angle object.
+
+        Parameters
+        ----------
+        index : int
+            The index of this Angle object
+        atom1_idx : int
+            The index of the first atom involved in this Angle
+        atom2_idx : int
+            The index of the second atom involved in this Angle
+        atom3_idx : int
+            The index of the third atom involved in this Angle
+        spring_constant : simtk.unit.Quantity
+            The spring constant of this Angle
+        eq_angle : simtk.unit.Quantity
+            The equilibrium angle of this Angle
+        """
         self._index = index
         self._atom1_idx = atom1_idx
         self._atom2_idx = atom2_idx
@@ -99,40 +270,118 @@ class Angle(TopologyElement):
         self._eq_angle = eq_angle
 
     def set_atom1_idx(self, index):
+        """
+        It sets atom1's index.
+
+        Parameters
+        ----------
+        index : int
+            The index of the first atom involved in this Angle
+        """
         self._atom1_idx = index
 
     def set_atom2_idx(self, index):
+        """
+        It sets atom2's index.
+
+        Parameters
+        ----------
+        index : int
+            The index of the second atom involved in this Angle
+        """
         self._atom2_idx = index
 
     def set_atom3_idx(self, index):
+        """
+        It sets atom3's index.
+
+        Parameters
+        ----------
+        index : int
+            The index of the third atom involved in this Angle
+        """
         self._atom3_idx = index
 
     @property
     def index(self):
+        """
+        Angle's index.
+
+        Returns
+        -------
+        index : int
+            The index of this Angle object
+        """
         return self._index
 
     @property
     def atom1_idx(self):
+        """
+        Angle's atom1 index.
+
+        Returns
+        -------
+        atom1_idx : int
+            The index of the first atom involved in this Angle object
+        """
         return self._atom1_idx
 
     @property
     def atom2_idx(self):
+        """
+        Angle's atom2 index.
+
+        Returns
+        -------
+        atom1_idx : int
+            The index of the second atom involved in this Angle object
+        """
         return self._atom2_idx
 
     @property
     def atom3_idx(self):
+        """
+        Angle's atom3 index.
+
+        Returns
+        -------
+        atom1_idx : int
+            The index of the third atom involved in this Angle object
+        """
         return self._atom3_idx
 
     @property
     def spring_constant(self):
+        """
+        Angle's spring constant.
+
+        Returns
+        -------
+        spring_constant : simtk.unit.Quantity
+            The spring constant of this Angle object
+        """
         return self._spring_constant
 
     @property
     def eq_angle(self):
+        """
+        Angle's equilibrium angle.
+
+        Returns
+        -------
+        eq_angle : simtk.unit.Quantity
+            The equilibrium angle of this Angle object
+        """
         return self._eq_angle
 
 
 class Dihedral(TopologyElement):
+    """
+    It represents a dihedral in the topology.
+
+    It can be a proper or an improper dihedral.
+    """
+
     _name = 'Dihedral'
     _writable_attrs = ['atom1_idx', 'atom2_idx', 'atom3_idx', 'atom4_idx',
                        'constant', 'prefactor', 'periodicity']
@@ -140,6 +389,28 @@ class Dihedral(TopologyElement):
     def __init__(self, index=-1, atom1_idx=None, atom2_idx=None,
                  atom3_idx=None, atom4_idx=None, periodicity=None,
                  prefactor=None, constant=None):
+        """
+        It initiates an Dihedral object.
+
+        Parameters
+        ----------
+        index : int
+            The index of this Dihedral object
+        atom1_idx : int
+            The index of the first atom involved in this Dihedral
+        atom2_idx : int
+            The index of the second atom involved in this Dihedral
+        atom3_idx : int
+            The index of the third atom involved in this Dihedral
+        atom4_idx : int
+            The index of the fourth atom involved in this Dihedral
+        periodicity : int
+            The periodicity of this Dihedral
+        prefactor : int
+            The prefactor of this Dihedral
+        constant : simtk.unit.Quantity
+            The constant of this Dihedral
+        """
         self._index = index
         self._atom1_idx = atom1_idx
         self._atom2_idx = atom2_idx
@@ -150,18 +421,53 @@ class Dihedral(TopologyElement):
         self._constant = constant
 
     def set_atom1_idx(self, index):
+        """
+        It sets atom1's index.
+
+        Parameters
+        ----------
+        index : int
+            The index of the first atom involved in this Dihedral
+        """
         self._atom1_idx = index
 
     def set_atom2_idx(self, index):
+        """
+        It sets atom2's index.
+
+        Parameters
+        ----------
+        index : int
+            The index of the second atom involved in this Dihedral
+        """
         self._atom2_idx = index
 
     def set_atom3_idx(self, index):
+        """
+        It sets atom3's index.
+
+        Parameters
+        ----------
+        index : int
+            The index of the third atom involved in this Dihedral
+        """
         self._atom3_idx = index
 
     def set_atom4_idx(self, index):
+        """
+        It sets atom4's index.
+
+        Parameters
+        ----------
+        index : int
+            The index of the fourth atom involved in this Dihedral
+        """
         self._atom4_idx = index
 
     def plot(self):
+        """
+        It plots this Dihedral as a function of phi angle.
+        """
         from matplotlib import pyplot
         import numpy as np
 
@@ -174,46 +480,122 @@ class Dihedral(TopologyElement):
 
     @property
     def index(self):
+        """
+        Dihedral's index.
+
+        Returns
+        -------
+        index : int
+            The index of this Dihedral object
+        """
         return self._index
 
     @property
     def atom1_idx(self):
+        """
+        Dihedral's atom1 index.
+
+        Returns
+        -------
+        atom1_idx : int
+            The index of the first atom involved in this Dihedral object
+        """
         return self._atom1_idx
 
     @property
     def atom2_idx(self):
+        """
+        Dihedral's atom2 index.
+
+        Returns
+        -------
+        atom1_idx : int
+            The index of the second atom involved in this Dihedral object
+        """
         return self._atom2_idx
 
     @property
     def atom3_idx(self):
+        """
+        Dihedral's atom3 index.
+
+        Returns
+        -------
+        atom1_idx : int
+            The index of the third atom involved in this Dihedral object
+        """
         return self._atom3_idx
 
     @property
     def atom4_idx(self):
+        """
+        Dihedral's atom4 index.
+
+        Returns
+        -------
+        atom1_idx : int
+            The index of the fourth atom involved in this Dihedral object
+        """
         return self._atom4_idx
 
     @property
     def periodicity(self):
+        """
+        Dihedral's periodicity.
+
+        Returns
+        -------
+        periodicity : int
+            The periodicity this Dihedral object
+        """
         return self._periodicity
 
     @property
     def prefactor(self):
+        """
+        Dihedral's prefactor.
+
+        Returns
+        -------
+        prefactor : int
+            The prefactor this Dihedral object
+        """
         return self._prefactor
 
     @property
     def constant(self):
+        """
+        Dihedral's constant.
+
+        Returns
+        -------
+        constant : int
+            The constant this Dihedral object
+        """
         return self._constant
 
 
 class Proper(Dihedral):
+    """
+    It represents a proper dihedral in the topology.
+    """
+
     _name = 'Proper'
 
 
 class Improper(Dihedral):
+    """
+    It represents an improper dihedral in the topology.
+    """
+
     _name = 'Improper'
 
 
 class OFFDihedral(TopologyElement):
+    """
+    It represents a dihedral in the Open Force Field's topology.
+    """
+
     _name = 'OFFDihedral'
     _writable_attrs = ['atom1_idx', 'atom2_idx', 'atom3_idx', 'atom4_idx',
                        'periodicity', 'phase', 'k', 'idivf']
@@ -222,6 +604,30 @@ class OFFDihedral(TopologyElement):
     def __init__(self, index=-1, atom1_idx=None, atom2_idx=None,
                  atom3_idx=None, atom4_idx=None, periodicity=None,
                  phase=None, k=None, idivf=None):
+        """
+        It initiates an Dihedral object.
+
+        Parameters
+        ----------
+        index : int
+            The index of this Dihedral object
+        atom1_idx : int
+            The index of the first atom involved in this Dihedral
+        atom2_idx : int
+            The index of the second atom involved in this Dihedral
+        atom3_idx : int
+            The index of the third atom involved in this Dihedral
+        atom4_idx : int
+            The index of the fourth atom involved in this Dihedral
+        periodicity : int
+            The periodicity of this Dihedral
+        phase : simtk.unit.Quantity
+            The phase of this Dihedral
+        k : simtk.unit.Quantity
+            The constant of this Dihedral
+        idivf : int
+            The idivf of this Dihedral
+        """
         self.index = index
         self.atom1_idx = atom1_idx
         self.atom2_idx = atom2_idx
@@ -233,6 +639,15 @@ class OFFDihedral(TopologyElement):
         self.idivf = idivf
 
     def to_PELE(self):
+        """
+        It converts this Open Force Field Dihedral object into a
+        PELE-compatible one.
+
+        Returns
+        -------
+        PELE_dihedral : a Dihedral
+            The PELE-compatible Dihedral object
+        """
         if (self.periodicity is None or self.phase is None
                 or self.k is None or self.idivf is None):
             return None
@@ -243,7 +658,7 @@ class OFFDihedral(TopologyElement):
         assert self.phase.value_in_unit(unit.degree) in (0, 180), \
             'Expected values for phase are 0 or 180, obtained ' \
             '{}'.format(self.phase)
-        assert self.idivf == 1, 'Expected values for idivf is 1, ' \
+        assert self.idivf == 1, 'The expected value for idivf is 1, ' \
             'obtained {}'.format(self.divf)
 
         if self.phase.value_in_unit(unit.degree) == 180:
@@ -266,6 +681,9 @@ class OFFDihedral(TopologyElement):
         return self._to_PELE_class(**PELE_dihedral_kwargs)
 
     def plot(self):
+        """
+        It plots this Dihedral as a function of phi angle.
+        """
         from matplotlib import pyplot
         import numpy as np
 
@@ -278,10 +696,18 @@ class OFFDihedral(TopologyElement):
 
 
 class OFFProper(OFFDihedral):
+    """
+    It represents a proper dihedral in the Open Force Field's topology.
+    """
+
     _name = 'OFFProper'
     _to_PELE_class = Proper
 
 
 class OFFImproper(OFFDihedral):
+    """
+    It represents an improper dihedral in the Open Force Field's topology.
+    """
+
     _name = 'OFFImproper'
     _to_PELE_class = Improper
