7b7fde43b26f795cce1be18bfe6c1006fd5b5c78
laumalo <laumaro95@gmail.com>
2020-11-02 12:36:41 +0100

    Add test_writer_OPLS

--- offpele/tests/test_templates.py
+++ offpele/tests/test_templates.py
@@ -4,7 +4,6 @@ This module contains the tests to check the offpele's Impact template builder.
 
 import pytest
 
-
 from offpele.utils import get_data_file_path
 from offpele.topology import Molecule
 from offpele.template import Impact
@@ -36,38 +35,34 @@ class TestImpactTemplate(object):
 
 		# Molecule is not a topology.molecule object
 		molecule_string= Molecule('non_molecule_type')
-		molecule_string.parameterize(FORCEFIELD_NAME)
 		with pytest.raises(TypeError):
 			impact = Impact(molecule_string)
 
 
 	def _compare_files(self, FILE1, FILE2): 
 		"""
-			It comapares two files line by line and gives an AssertionError if there 
-			is any difference. 
+		It comapares two files line by line and gives an AssertionError if there 
+		is any difference. 
 		"""
 		with open(FILE1, 'r') as f1: 
-			dataA = f1.readlines()[3:]
+			linesA = [line for line in f1.readlines() if not line.startswith("*")]
 		with open(FILE2, 'r') as f2: 
-			dataB = f2.readlines()[3:]
-		linesA = str(dataA).split('\n')
-		linesB = str(dataB).split('\n')
+			linesB = [line for line in f2.readlines() if not line.startswith("*")]
 		assert len(linesA) == len(linesB), 'Number of lines do not match'
 		for i, (lineA, lineB) in enumerate(zip(linesA, linesB)):
-			assert lineA == lineB, 'Found different lines at line {}:' + lineA + ' - ' + lineB
+			assert lineA == lineB, 'Found different lines at line {}:'.format(i) + '\n' + lineA  + lineB
 
-	def test_writer(self):
+	def test_writer_OFF(self):
 		"""
-		It tests the writer attribute of the Impact class. 
+		It tests the writer attribute of the Impact class using OFF. 
 		"""
-
 		FORCEFIELD_NAME = 'openff_unconstrained-1.1.1.offxml'
 
 		TEMPLATE_METZ = get_data_file_path('tests/metz')
 		TEMPLATE_MATZ = get_data_file_path('tests/malz')
 		TEMPLATE_ETLZ = get_data_file_path('tests/etlz')
 
-		# Generates the tempalte for methane
+		# Generates the template for methane
 		pdb_path = get_data_file_path('ligands/methane.pdb')
 		m = Molecule(pdb_path)
 		m.parameterize('openff_unconstrained-1.2.1.offxml')
@@ -97,6 +92,58 @@ class TestImpactTemplate(object):
 		# Compare the reference template and the generated template
 		self._compare_files(FILE1 = TEMPLATE_ETLZ, FILE2 = 'etlz')
 
+	def test_writer_OPLS(self): 
+		"""
+		It tests the writer attribute of the Impact class using OPLS. 
+		"""
+		from offpele.forcefield import (OPLS2005ForceField,
+										OPLS2005ParameterWrapper)
+		FORCE_FIELD_NAME = 'OPLS2005'
+
+		TEMPLATE_METZ_OPLS = get_data_file_path('tests/OPLS_metz')
+		TEMPLATE_ETLZ_OPLS = get_data_file_path('tests/OPLS_etlz')
+
+		# Generates the template for methane
+		pdb_path = get_data_file_path('ligands/methane.pdb')
+		m = Molecule(pdb_path)
+		oplsff = OPLS2005ForceField(FORCE_FIELD_NAME)
+		
+		# Set force field and obtain parameters
+		ffld_file = get_data_file_path('tests/MET_ffld_output.txt')
+		with open(ffld_file) as f:
+			ffld_output = f.read()
+
+		# Set force field and obtain parameters
+		m.set_forcefield(oplsff)
+		m._parameters = \
+			OPLS2005ParameterWrapper.from_ffld_output(m, ffld_output)
+		m.parameterize()
+		impact = Impact(m)
+		impact.write('metz')
+
+		# Compare the reference template and the generated template
+		self._compare_files(FILE1 = TEMPLATE_METZ_OPLS, FILE2 = 'metz')
+
+		# Generates the template for ethylene
+		pdb_path = get_data_file_path('ligands/ethylene.pdb')
+		m = Molecule(pdb_path, tag='ETL')
+		oplsff = OPLS2005ForceField(FORCE_FIELD_NAME)
+		
+		# Set force field and obtain parameters
+		ffld_file = get_data_file_path('tests/ETL_ffld_output.txt')
+		with open(ffld_file) as f:
+			ffld_output = f.read()
+
+		# Set force field and obtain parameters
+		m.set_forcefield(oplsff)
+		m._parameters = \
+			OPLS2005ParameterWrapper.from_ffld_output(m, ffld_output)
+		m.parameterize()
+		impact = Impact(m)
+		impact.write('etlz')
+
+		# Compare the reference template and the generated template
+		self._compare_files(FILE1 = TEMPLATE_ETLZ_OPLS, FILE2 = 'etlz')
 
 
 
