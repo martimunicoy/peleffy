36af563af42d57e7606db0bc6f95ccf0ad9eb395
Laura Malo <44496034+laumalo@users.noreply.github.com>
2021-02-09 17:03:51 +0100

    Add method parameters from JSON

--- peleffy/utils/utils.py
+++ peleffy/utils/utils.py
@@ -11,6 +11,7 @@ __all__ = ["get_data_file_path",
            "unit_to_string",
            "quantity_to_string",
            "convert_all_quantities_to_string",
+           "string_to_quantity",
            "parse_charges_from_mae",
            "Logger"
            ]
@@ -223,6 +224,72 @@ def convert_all_quantities_to_string(data_structure):
         obj_to_return = data_structure
 
     return obj_to_return
+
+
+def _ast_eval(node):
+    """
+    Performs an algebraic syntax tree evaluation of a unit.
+
+    Function modified from the OpenFF Toolkit
+    (https://github.com/openforcefield/openforcefield/).
+
+    Parameters
+    ----------
+    node : An ast parsing tree node
+    """
+    import ast
+    import operator as op
+
+    operators = {
+        ast.Add: op.add,
+        ast.Sub: op.sub,
+        ast.Mult: op.mul,
+        ast.Div: op.truediv,
+        ast.Pow: op.pow,
+        ast.BitXor: op.xor,
+        ast.USub: op.neg, }
+
+    if isinstance(node, ast.Num):  # <number>
+        return node.n
+    elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
+        return operators[type(node.op)](_ast_eval(node.left),
+                                        _ast_eval(node.right))
+    elif isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
+        return operators[type(node.op)](_ast_eval(node.operand))
+    elif isinstance(node, ast.Name):
+        # see if this is a simtk unit
+        b = getattr(unit, node.id)
+        return b
+    elif isinstance(node, ast.List):
+        return ast.literal_eval(node)
+    else:
+        raise TypeError(node)
+
+
+def string_to_quantity(quantity_string):
+    """
+    Takes a string representation of a quantity and returns a unit.Quantity
+
+    Function modified from the OpenFF Toolkit
+    (https://github.com/openforcefield/openforcefield/).
+
+    Parameters
+    ----------
+    quantity_string : str
+        The quantity to deserialize
+
+    Returns
+    -------
+    output_quantity : simtk.unit.Quantity
+        The deserialized quantity
+    """
+    import ast
+
+    if quantity_string is None:
+        return None
+
+    output_quantity = _ast_eval(ast.parse(quantity_string, mode="eval").body)
+    return output_quantity
 
 
 def parse_charges_from_mae(path, parameters):
