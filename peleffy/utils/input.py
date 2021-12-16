438ad982260f50c1a37bef5dd6f0ed27fb6c5ea3
Martí Municoy <martimunicoy@gmail.com>
2021-12-16 19:21:50 +0100

    Minor changes and release history update

--- peleffy/utils/input.py
+++ peleffy/utils/input.py
@@ -104,10 +104,15 @@ class PDBFile(object):
 
                 if line.startswith('CONECT'):
                     stripped_line = line.replace("CONECT", "")
-                    ids_in_line = [stripped_line[i:i + 5] for i in range(0, len(stripped_line), 5)]
-                    stripped_ids_in_line = [element.strip() for element in ids_in_line]
+                    ids_in_line = [stripped_line[i:i + 5] for i in
+                                   range(0, len(stripped_line), 5)]
 
-                    if any([atom_id in stripped_ids_in_line for atom_id in atom_ids]):
+                    # Strip out whitespaces from ids
+                    stripped_ids_in_line = [element.strip() for
+                                            element in ids_in_line]
+
+                    if any([atom_id in stripped_ids_in_line for
+                            atom_id in atom_ids]):
                         pdb_block.append(line)
 
             try:
